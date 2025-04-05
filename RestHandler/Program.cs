29b7985472d074e6404e45c219b6c76a35ff4342
Gayane03 <111762021+Gayane03@users.noreply.github.com>
2025-04-05 14:01:55 +0400

    added new services

--- RestHandler/Program.cs
+++ RestHandler/Program.cs
@@ -1,3 +1,6 @@
+using RestHandler.ClientServices;
+using RestHandler.Helper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<PostsClientService>(BackendApi.Posts, client =>
+{
+	client.BaseAddress = new Uri(builder.Configuration.GetSection("ApiUrls")[BackendApi.Posts]!);
+});
+
+builder.Services.AddHttpClient<UsersClientService>(BackendApi.Users, client =>
+{
+	client.BaseAddress = new Uri(builder.Configuration.GetSection("ApiUrls")[BackendApi.Users]!);
+});
+
+builder.Services.AddScoped<IPostsClientService, PostsClientService>();
+builder.Services.AddScoped<IUsersClientService, UsersClientService>();
+builder.Services.AddScoped<IResponseMessageUtile, ResponseMessageUtile>();
 
 var app = builder.Build();
 
