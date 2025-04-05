956eafc0dd3f0329f762591082c20a8426be5b63
Gayane03 <111762021+Gayane03@users.noreply.github.com>
2025-04-05 13:59:40 +0400

    added new ClientServices

---
+++ RestHandler/ClientServices/IPostsClientService.cs
@@ -0,0 +1,11 @@
+﻿using RestHandler.Models;
+
+namespace RestHandler.ClientServices
+{
+	public interface IPostsClientService
+	{
+	    Task<HttpResponseMessage> GetPostsWithFilter(PostFilter postFilter);
+		Task<HttpResponseMessage> GetPost(int id);
+		Task<HttpResponseMessage> DeletePost(int id);
+	}
+}
---
+++ RestHandler/ClientServices/PostsClientService.cs
@@ -0,0 +1,35 @@
+﻿using Flurl;
+using RestHandler.Helper;
+using RestHandler.Models;
+
+namespace RestHandler.ClientServices
+{
+	public class PostsClientService : IPostsClientService
+	{
+		private readonly HttpClient httpClient;
+
+		public PostsClientService(IHttpClientFactory httpClientFactory)
+		{
+			this.httpClient = httpClientFactory.CreateClient(BackendApi.Posts);
+		}
+
+		public async Task<HttpResponseMessage> GetPostsWithFilter(PostFilter postFilter)
+		{
+			var uri = "posts".SetQueryParams(postFilter);
+			return await httpClient.GetAsync(uri);
+		}
+
+		public async Task<HttpResponseMessage> GetPost(int id)
+		{
+			var uri = $"posts/{id}";
+			return await httpClient.GetAsync(uri);
+		}
+
+		public async Task<HttpResponseMessage> DeletePost(int id)
+		{
+			var uri = $"posts/{id}";
+			return await httpClient.DeleteAsync(uri);
+		}
+
+	}
+}
